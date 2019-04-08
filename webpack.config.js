b5044f8174e9e5016a56893bcfd80670a23f7b9c
110619036 <43201503+110619036@users.noreply.github.com>
2019-04-08 18:22:02 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+let path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackagePlugin=require('html-webpack-plugin');
+module.exports = {
+    entry : './js/script.js',
+    output:{
+        path:path.join(__dirname,'dist'),
+        filename:'bundle.js',
+        publicPath:'./'
+    },
+    module:{
+        rules:[
+                {
+                test: /\.css$/,
+                use:ExtractTextPlugin.extract({
+                    use:[{
+                        loader:'css-loader',
+                        options:{
+                            url:false}
+                        },
+                        {
+                            loader:'postcss-loader'
+                        }]
+                    }),
+                
+                },
+                {
+                    test: /\.(jpe?g|png|gif|svg)$/,
+                    use: [
+                       {
+                          loader: 'url-loader',
+                          options: {
+                             limit: 40000,
+                             outputPath: './images',
+                             publicPath: './images'
+                          }
+                       },
+                       'image-webpack-loader'
+                    ]
+                },
+        
+
+            ],
+
+        },
+    plugins:[
+        new ExtractTextPlugin('css/main.css'),
+        new HtmlWebpackagePlugin({
+            template:'./index.html',
+        })
+    ]
+    
+
+}
